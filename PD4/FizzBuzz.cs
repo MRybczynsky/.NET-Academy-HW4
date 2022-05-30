763aa0d53b75a2769b74673f00df3a205c48f728
Maciej Rybczyński <rybczynskimaciek@gmail.com>
2022-05-30 19:05:55 +0200

    Add project files.

---
+++ PD4/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD4
+{
+    class FizzBuzz
+    {
+        public string GetOutput(int number)
+        {
+            if (number % 3 == 0)
+            {
+                if (number % 5 == 0)
+                    return "FizzBuzz";
+                else
+                    return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+                return number.ToString();
+        }
+    }
+}
---
+++ PD4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra FizzBuzz");
+            var FizzBuzz = new FizzBuzz();
+            Console.WriteLine("Wprowadź liczbę:");
+            while (true)
+            {
+                var number = getNum();
+                Console.WriteLine(FizzBuzz.GetOutput(number));
+                Console.WriteLine("Podaj następną liczbę lub zakończ (t)");
+            }
+            
+        }
+
+        private static int getNum()
+        {
+            var val = Console.ReadLine();
+            if (val.ToUpper() == "T")
+                Environment.Exit(0);
+            while (true)
+            {
+                if (!int.TryParse(val, out int num))
+                {
+                    Console.WriteLine("Podałeś nieprawidlowe dane. Podaj inna liczbę");
+                    continue;
+                }
+                return num;
+            }
+        }
+    }
+}
